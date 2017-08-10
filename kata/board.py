112230d41481a0259fa07de8d1fb243810ba253f
jorge jardines <jorge@themotion.com>
2017-08-11 00:27:23 +0200

    add test for adding columns, mutable args example

--- kata/board.py
+++ kata/board.py
@@ -1,8 +1,16 @@
 class Board:
-    def __init__(self, title, is_public=True):
+    def __init__(self, title, is_public=True, columns=[]):
         self.title = title
         self.is_public = is_public
         self.tags = []
+        self._columns = columns
 
     def add_tags(self, tags):
         pass
+
+    def add_column(self, column):
+        self._columns.append(column)
+
+    @property
+    def columns(self):
+        return self._columns
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,38 +1,54 @@
 from kata.board import Board
 from unittest import TestCase
 
+TITLE = 'irrelevant_title'
+
 
 class TestBoard(TestCase):
 
     def test_is_public(self):
-        board = Board(title='irrelevant_title', is_public=True)
+        board = Board(title=TITLE, is_public=True)
         assert isinstance(board, Board)
         assert board.is_public
 
     def test_is_not_public_when_false(self):
-        board = Board(title='irrelevant_title', is_public=False)
+        board = Board(title=TITLE, is_public=False)
         assert isinstance(board, Board)
         assert board.is_public is False
 
     def test_is_not_public_when_None(self):
-        board = Board(title='irrelevant_title', is_public=None)
+        board = Board(title=TITLE, is_public=None)
         assert isinstance(board, Board)
         assert board.is_public is False
 
     def test_add_tag_list(self):
-        board = Board(title='irrelevant_title')
+        board = Board(title=TITLE)
         tags = ['tag_1', 'tag_2', 'tag_3']
         board.add_tags(tags)
         assert board.tags == tags
 
     def test_add_single_tag(self):
-        board = Board(title='irrelevant_title')
+        board = Board(title=TITLE)
         tag = 'tag_1'
         board.add_tags(tag)
         assert board.tags == [tag]
 
     def test_add_none_tags(self):
-        board = Board(title='irrelevant_title')
+        board = Board(title=TITLE)
         board.add_tags('')
         board.add_tags(None)
         assert board.tags == []
+
+    def test_columns(self):
+        board1 = Board(title=TITLE, columns=['ToDo', 'Done'])
+
+        board2 = Board(title=TITLE)
+        board3 = Board(title=TITLE)
+
+        board2.add_column('Doing')
+        board3.add_column('OnHold')
+        board2.add_column('Archived')
+
+        assert board1.columns == ['ToDo', 'Done']
+        assert board2.columns == ['Doing', 'Archived']
+        assert board3.columns == ['OnHold']
